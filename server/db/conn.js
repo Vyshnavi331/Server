484b7e8ae54f242acbe45b225dbdb62a2c69eb86
Vyshnavi331 <86727071+Vyshnavi331@users.noreply.github.com>
2022-11-24 23:22:54 +0530

    Delete server directory

--- server/db/conn.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require("mongoose");
-
-const DB="mongodb+srv://BePositive:BePositive@cluster0.twnyx.mongodb.net/Bepositive?retryWrites=true&w=majority"
-
-mongoose.connect(DB,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}).then(()=>console.log("Connection start")).catch((error)=>console.log(error.message));
